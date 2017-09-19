dff30969607bb5b440b60e9d10253207674f0d4d
anyuke <1249836965@qq.com>
2017-09-19 19:42:46 +0800

    定时刷新微信jsapi票据

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 var schedule = require('node-schedule');
 
+logger.info('刷新微信JSAPI票据定时任务，每小时运行');
 var rule = new schedule.RecurrenceRule();
-rule.hour = 0;
 rule.minute = 0;
 rule.second = 0;
 schedule.scheduleJob(rule, function() {
-    var filePath = '/root/playback/js3mj/' + utils.dateFormat(new Date(new Date().getTime() - 60 * 60 * 24 * 15 * 1000), 'yyyy-MM-dd');
-    if (fs.existsSync(filePath)) {
-        logger.info('删除文件夹:', filePath);
-        exec('rm -rf ' + filePath);
-    }
+    request.post('https://api.weixin.qq.com/cgi-bin/token', {form: {grant_type: 'client_credential', appid: config.third.weixin.appId, secret: config.third.weixin.appSecret}}, function(err, rsp, body) {
+        if (err) {
+            logger.error(err);
+            return;
+        }
+        var accessToken = JSON.parse(body).access_token;
+        if (accessToken) {
+            request.post('https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=ACCESS_TOKEN&type=jsapi', {form: {access_token: accessToken, type: 'jsapi'}}, function(err, rsp, body) {
+                var ticket = JSON.parse(body).ticket;
+                if (ticket) {
+                    redisUtil.client().setex(config.weixinTicketPrefix, config.weixinTicketExpireTime, ticket);
+                }
+            });
+        }
+    });
 });
